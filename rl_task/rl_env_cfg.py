9a69564717ff5e7086b55d54521b483a0e9574fa
Aashish Dhanani <aashishd2004@gmail.com>
2026-01-11 13:52:37 -0600

    same as last commit, forgot to save some code

--- rl_task/rl_env_cfg.py
+++ rl_task/rl_env_cfg.py
@@ -5,3 +5,14 @@ rewards
 terminations
 resets
 '''
+
+
+# observation = [
+#   agent_state,        # pose, velocity, joints
+#   agent_dynamics,     # velocities, angular rates
+#   env_state,          # objects, obstacles
+#   relative_state,     # object-to-agent, goal-to-object
+#   goal_state,         # target, command
+# ]
+class Observations:
+    scene["so101"]
