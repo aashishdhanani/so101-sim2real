c6ddfed6f66e8cddcbee7b39de46e9eab73530bf
Aashish Dhanani <aashishd2004@gmail.com>
2026-01-18 16:20:55 -0600

    update: added init import to train for gym.make()

--- rl_task/train.py
+++ rl_task/train.py
@@ -22,6 +22,7 @@ from rsl_rl.runners import OnPolicyRunner
 from agents.rsl_rl_ppo_cfg import So101PPORunnerCfg
 from manager_rl_env import So101EnvCfg
 from isaaclab.envs import ManagerBasedRLEnv
+import rl_task
 
 def main():
     # Create environment config (override num_envs if specified)
